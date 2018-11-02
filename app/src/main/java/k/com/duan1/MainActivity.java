76241b8e0ba7a53c8ab9ef6560cc9d8d75235c2d
KaiBosss <tiokai97@gmai.com>
2018-11-02 16:43:19 +0700

    TU CHUYEN DOI MAN HINH CHAO

--- app/src/main/java/k/com/duan1/MainActivity.java
+++ app/src/main/java/k/com/duan1/MainActivity.java
@@ -1,5 +1,6 @@
 package k.com.duan1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
         //asdas
         //dasdasd
         //lll
+        //Dùng cài đặt sau 5 giây màn hình tự chuyển
+        Thread bamgio=new Thread(){
+            public void run()
+            {
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+
+                }
+                finally
+                {
+                    Intent activitymoi=new Intent(MainActivity.this,ManHinhChinh.class);
+                    startActivity(activitymoi);
+                }
+            }
+        };
+        bamgio.start();
     }
-}
+    //sau khi chuyển sang màn hình chính, kết thúc màn hình chào
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+    }
+
---
+++ app/src/main/java/k/com/duan1/ManHinhChinh.java
@@ -0,0 +1,14 @@
+package k.com.duan1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManHinhChinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chinh);
+    }
+}
