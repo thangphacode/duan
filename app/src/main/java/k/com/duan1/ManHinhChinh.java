46f8f496482c5b46931b67ee50c204650fe924ae
KaiBosss <tiokai97@gmai.com>
2018-11-02 17:50:41 +0700

    cap nhat anhr nen man hinh chao

--- app/src/main/java/k/com/duan1/ManHinhChinh.java
+++ app/src/main/java/k/com/duan1/ManHinhChinh.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ManHinhChinh extends AppCompatActivity {
-
+//kkkkkkkkkkkkkkkkk
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
